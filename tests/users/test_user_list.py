c7a19d4cd1efdf9349be35f66626d5f2f4dd4654
Breno-MT <hentgenp@gmail.com>
2022-09-27 00:23:29 -0300

    [BRENO] Done Test User List

--- tests/users/test_user_list.py
+++ tests/users/test_user_list.py
@@ -1,6 +1,7 @@
 from flask import json
 
-def test_list_user(client, logged_in_client):
+
+def test_list_user_success(client, logged_in_client):
 
     mimetype = 'application/json'
     headers = {
@@ -12,4 +13,67 @@ def test_list_user(client, logged_in_client):
 
     response = client.get("user/", headers=headers)
 
-    assert response.json
+    assert response.status_code == 200
+
+
+def test_list_user_insufficient_permission(client):
+
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype
+    }
+
+    response = client.get('user/', headers=headers)
+
+    assert response.status_code == 403
+
+
+def test_list_one_user_by_name(client, logged_in_client):
+
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        "Authorization": f"Bearer {logged_in_client}"
+    }
+
+    name = "L"
+
+    response = client.get(f"user/?name={name}", headers=headers)
+
+    assert response.status_code == 200
+
+
+def test_list_many_users_by_name(client, logged_in_client):
+
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        "Authorization": f"Bearer {logged_in_client}"
+    }
+
+    name = "a"
+
+    response = client.get(f"user/?name={name}", headers=headers)
+
+    assert response.status_code == 200
+
+
+def test_list_user_no_content(client, logged_in_client):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        "Authorization": f"Bearer {logged_in_client}"
+    }
+
+    name = "ZZZZZZZZZZZZZ"
+
+    response = client.get(f"user/?name={name}", headers=headers)
+
+    assert response.status_code == 204
+
+
+
