43801dd4ed40d69a9aaef23661757bd55192fe96
Breno-MT <hentgenp@gmail.com>
2022-09-27 13:31:13 -0300

    [BRENO] Done Test Update User

---
+++ tests/users/test_user_update.py
@@ -0,0 +1,85 @@
+import random
+from flask import json
+
+def test_update_user_success(client, logged_in_as_root):
+    
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    user_id = random.randint(1,8)
+
+    data = {
+        "email": "teste1111@gmail.com",
+        "name": "TESTE",
+        "password": "123TEstando!!"
+    }
+
+    response = client.patch(f"user/{user_id}", data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 204
+
+def test_update_user_permission_insufficient(client, logged_in_client):
+
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        'Authorization': f"Bearer {logged_in_client}"
+    }
+
+    user_id = random.randint(1,8)
+
+    data = {
+        "email": "teste1111@gmail.com",
+        "name": "TESTE",
+        "password": "123TEstando!!"
+    }
+
+    response = client.patch(f"user/{user_id}", data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 403
+
+def test_update_user_with_empty_body(client, logged_in_as_root):
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        'Authorization': f"Bearer {logged_in_as_root}"
+    }
+
+    user_id = random.randint(1,8)
+
+    data = {
+        "email": "",
+        "name": "teste",
+        "password": "123TEstando!!"
+    }
+
+    response = client.patch(f"user/{user_id}", data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 400
+
+def test_update_user_not_found(client, logged_in_as_root):
+    
+    mimetype = 'application/json'
+    headers = {
+        'Content-Type': mimetype,
+        'Accept': mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    user_id = random.randint(100,900)
+
+    data = {
+        "email": "teste1111@gmail.com",
+        "name": "TESTE",
+        "password": "123TEstando!!"
+    }
+
+    response = client.patch(f"user/{user_id}", data=json.dumps(data), headers=headers)
+
+    assert response.status_code == 404
