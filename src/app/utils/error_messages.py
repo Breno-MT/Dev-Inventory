1d9f85a30f908019d4bff4a0def57966b201563e
Breno-MT <hentgenp@gmail.com>
2022-09-28 14:49:15 -0300

    [BRENO] Improved error logic

--- src/app/utils/error_messages.py
+++ src/app/utils/error_messages.py
@@ -2,7 +2,7 @@
 def handle_error_messages(field):
     error_message = {
         'required': f'O campo {field} é obrigatório.',
-        'invalid': 'Não é um campo válido.'
+        'invalid': f'{field} Não é um campo válido.'
     }
 
     return error_message
