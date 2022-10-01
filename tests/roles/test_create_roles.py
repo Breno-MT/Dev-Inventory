ca219dc032be37d7049ad5d6037bd60adcc370ef
Breno-MT <hentgenp@gmail.com>
2022-09-30 21:26:08 -0300

    [BRENO] Refact test_create_roles

--- tests/roles/test_create_roles.py
+++ tests/roles/test_create_roles.py
@@ -16,7 +16,7 @@ def test_create_role_success(client, logged_in_as_root):
 
     response = client.post("user/role", data=json.dumps(data), headers=headers)
 
-
+    assert response.json["message"] == "Role criada com sucesso."
     assert response.status_code == 201
 
 
@@ -57,7 +57,6 @@ def test_create_role_missing_array_of_permission(client, logged_in_as_root):
 
     for error in response.json:
         assert response.json[error] == [f'O campo {error} é obrigatório.']
-    
     assert response.status_code == 400
 
 
@@ -70,7 +69,7 @@ def test_create_role_already_exists(client, logged_in_as_root):
     }
 
     data = {
-        "description": "Brabo",
+        "description": "Braboo",
         "name": "O mais brabo",
         "permissions": [1, 2, 3, 4]
     }
@@ -91,7 +90,7 @@ def test_create_role_with_non_existent_permission(client, logged_in_as_root):
     }
 
     data = {
-        "description": "Teste",
+        "description": "Testee",
         "name": "Se funcionar, deu ruim demais",
         "permissions": [5, 5]
     }
@@ -110,7 +109,7 @@ def test_create_role_with_non_existent_and_existent_permissions(client, logged_i
     }
 
     data = {
-        "description": "Teste",
+        "description": "Testee",
         "name": "Se funcionar, deu ruim demais",
         "permissions": [1, 5, 6, 2, 10, 12, 3]
     }
@@ -131,7 +130,7 @@ def test_create_role_with_invalid_description_format(client, logged_in_as_root):
 
     data = {
         "description": 1234,
-        "name": "1234",
+        "name": "testeee",
         "permissions": [1,2,3]
     }
 
@@ -139,11 +138,10 @@ def test_create_role_with_invalid_description_format(client, logged_in_as_root):
 
     for error in response.json:
         assert response.json[error] == [f'{error} Não é um campo válido.']
-
     assert response.status_code == 400
 
 
-def teste_create_role_with_zero_permissions(client, logged_in_as_root):
+def test_create_role_with_zero_permissions(client, logged_in_as_root):
     mimetype = "application/json"
     headers = {
         "Content-Type": mimetype,
@@ -161,7 +159,89 @@ def teste_create_role_with_zero_permissions(client, logged_in_as_root):
 
     for error in response.json:
         assert response.json[error] == [f'O {error} não pode ser menor ou igual a 0.']
-    
     assert response.status_code == 400
 
+
+def test_create_role_with_length_description_lower_than_5(client, logged_in_as_root):
+    mimetype = "application/json"
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    data = {
+        "description": "t",
+        "name": "testet",
+        "permissions": [1,2,3,4]
+    }
+
+    response = client.post("user/role", data=json.dumps(data), headers=headers)
+
+    for error in response.json:
+        assert response.json[error] == [f'{error} deve ser apenas String e acima de 5 caracteres.']
+    assert response.status_code == 400
+
+def test_create_role_with_invalid_name_format(client, logged_in_as_root):
+    mimetype = "application/json"
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    data = {
+        "description": "É Pra falhar isso aque",
+        "name": 123,
+        "permissions": [1,2,3]
+    }
+
+    response = client.post("user/role", data=json.dumps(data), headers=headers)
+
+    for error in response.json:
+        assert response.json[error] == [f'{error} Não é um campo válido.']
+    assert response.status_code == 400
+
+
+def test_create_with_length_name_lower_than_5(client, logged_in_as_root):
+    mimetype = "application/json"
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    data = {
+        "description": "Testeee",
+        "name": "test",
+        "permissions": [1,2,3]
+    }
+
+    response = client.post("user/role", data=json.dumps(data), headers=headers)
+
+    for error in response.json:
+        assert response.json[error] == [f'{error} deve ser apenas String e acima de 5 caracteres.']
+    assert response.status_code == 400
+
+
+def test_create_role_invalid_permission_format(client, logged_in_as_root):
+    mimetype = "application/json"
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Authorization": f"Bearer {logged_in_as_root}"
+    }
+
+    data = {
+        "description": "Testeee",
+        "name": "testee",
+        "permissions": 123
+    }
+
+    response = client.post("user/role", data=json.dumps(data), headers=headers)
+
+    for error in response.json:
+        assert response.json[error] == [f'{error} Não é um campo válido.']
+    
+    assert response.status_code == 400
 
